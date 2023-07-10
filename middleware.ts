5512a43ac11e5eb8f7f6ac2b8d1b5e23efdd0f52
DominickVale <dominickveil@gmail.com>
2023-07-10 16:50:36 +0200

    Update env and middleware with bot/useragent stuff

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,14 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
+
+const userAgentReg = new RegExp(
+    process.env.ALLOWED_USER_AGENT_REGEX || /^NEUROTAP-v0\.2-BEG!---.*\d+!---$/
+)
 
 export async function middleware(req: NextRequest) {
     const userAgent = req.headers.get("user-agent")
-    //                                                   don't forget this lol
-    if (req.nextUrl.pathname.startsWith('/c2/panel')/* && userAgent != "test"*/) {
+    if (!req.cookies.get("t")) return NextResponse.rewrite(new URL('/login', req.url))
+    if (req.nextUrl.pathname.startsWith('/c2/panel') && typeof userAgent === 'string' && userAgentReg.test(userAgent)) {
         return NextResponse.rewrite(new URL('/error', req.url));
     }
 }
