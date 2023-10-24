d0002e3dc5f519b86f953031c15f204a42570521
Dominick Vale <dominickveil@gmail.com>
2023-10-24 08:47:15 +0200

    fix 3d brain functionality

--- challenge/web/components/Brain.tsx
+++ challenge/web/components/Brain.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React, { useEffect, useLayoutEffect, useRef } from "react";
+import { Float } from "@react-three/drei";
 import { Canvas, extend, useFrame, useLoader, useThree } from "@react-three/fiber";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/all";
 import { useControls } from "leva";
+import { useMount } from "react-use";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
@@ -19,61 +20,52 @@ const floatingRange = [-0.1, 0.1];
 
 function BrainModel() {
     const brainRef = useRef<THREE.Mesh | null>(null);
-    const { xPos, yPos, zPos } = useControls({
-        xPos: debugRot,
-        yPos: debugRot,
-        zPos: debugRot,
-    });
-    const planeRef = useRef<THREE.Plane>(new THREE.Plane(new THREE.Vector3(0,0,1), window.innerWidth));
+    // const { xPos, yPos, zPos } = useControls({
+    //     xPos: debugRot,
+    //     yPos: debugRot,
+    //     zPos: debugRot,
+    // });
+    const planeRef = useRef<THREE.Plane>(new THREE.Plane(new THREE.Vector3(0, 0, 1), -200));
     const raycasterRef = useRef(new THREE.Raycaster());
     const pointOfIntersectionRef = useRef(new THREE.Vector3());
+    const mouseRef = useRef(new THREE.Vector2());
+    const rotationAppliedRef = useRef(false);
 
     const { camera, gl } = useThree();
     const { nodes, ...gltf } = useLoader(GLTFLoader, "/brain.gltf");
 
-    const offset = React.useRef(Math.random() * 10000);
+    function handleMouseMove(e: MouseEvent) {
+        mouseRef.current.x = (e.clientX / window.innerWidth) * 2 - 1;
+        mouseRef.current.y = -((e.clientY / window.innerHeight) * 2 - 1);
+    }
 
-    console.log("Rerendered", camera);
-    
-    // useEffect(() => {
-    //     window.addEventListener("mousemove", (event) => {
-    //         if (!brainRef.current || !planeRef.current) return;
-    //         const mouse = new THREE.Vector2();
-    //         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    //         mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    //         raycasterRef.current.setFromCamera(mouse, camera);
-    //         raycasterRef.current.ray.intersectPlane(
-    //             planeRef.current,
-    //             pointOfIntersectionRef.current
-    //         );
-    //         brainRef.current.lookAt(pointOfIntersectionRef.current);
-    //     });
-    // }, []);
-    
     useEffect(() => {
+        // "hack" since useFrame's mouse doesn't update when canvas loses focus.
+        window.addEventListener("mousemove", handleMouseMove);
         if (nodes) {
             const brainGeometry = (nodes["brain"] as THREE.Mesh).geometry;
-            if (brainGeometry) {
-                // brainGeometry.applyMatrix4(new THREE.Matrix4().makeRotationY(Math.PI / 2));
+            if (brainGeometry && !rotationAppliedRef.current) {
+                brainGeometry.applyMatrix4(new THREE.Matrix4().makeRotationY(Math.PI / 2));
+                // needed to prevent unneeded rotation after hot reload
+                rotationAppliedRef.current = true;
             }
         }
-    }, [nodes]);
 
-    useFrame(({ mouse, clock }) => { 
-        if (brainRef.current && planeRef.current) {
-            raycasterRef.current.setFromCamera(mouse, camera);
-            raycasterRef.current.ray.intersectPlane(
-                planeRef.current,
-                pointOfIntersectionRef.current
-            );
-            console.log("pointOfIntersectionRef: ", pointOfIntersectionRef.current);
-            
-            brainRef.current.lookAt(
-               pointOfIntersectionRef.current.x,
-                pointOfIntersectionRef.current.y,
-               - pointOfIntersectionRef.current.z
-            );
-        }
+        return () => window.removeEventListener("mousemove", handleMouseMove);
+    }, []);
+
+    useFrame(({ clock }) => {
+        if (!brainRef.current || !mouseRef.current) return;
+
+        // Intersect with plane
+        raycasterRef.current.setFromCamera(mouseRef.current, camera);
+        raycasterRef.current.ray.intersectPlane(planeRef.current, pointOfIntersectionRef.current);
+
+        brainRef.current.lookAt(
+            pointOfIntersectionRef.current.x,
+            pointOfIntersectionRef.current.y,
+            pointOfIntersectionRef.current.z
+        );
     });
 
     useLayoutEffect(() => {
@@ -82,50 +74,45 @@ function BrainModel() {
 
             const animation = gsap.timeline();
 
-            const brainPosScrollTrigger: ScrollTrigger.Vars = {
-                trigger: "#hero",
-                start: "-5% top",
-                end: "bottom top",
-                immediateRender: false,
-                scrub: 1,
-            };
             animation.to(brainRef.current.position, {
                 x: -152,
-                y: 13,
-                z: -350,
                 duration: 0.5,
                 ease: "linear",
-                scrollTrigger: brainPosScrollTrigger,
-            });
-            animation.to(
-                brainRef.current.rotation,
-                {
-                    z: degToRad(180),
-                    duration: 0.5,
-                    ease: "linear",
-                    scrollTrigger: brainPosScrollTrigger,
+                scrollTrigger: {
+                    trigger: "#hero",
+                    start: "-5% top",
+                    end: "bottom top",
+                    immediateRender: false,
+                    scrub: 1,
                 },
-                "<"
-            );
+            });
+            animation.to(brainRef.current.position, {
+                x: 210,
+                duration: 0.5,
+                ease: "linear",
+                scrollTrigger: {
+                    trigger: "footer",
+                    start: "top bottom",
+                    end: "top top",
+                    immediateRender: false,
+                    scrub: true,
+                },
+            });
         });
 
         return () => ctx.revert();
     }, [camera]);
 
     return (
-        <>
+        <Float speed={3} rotationIntensity={0.1} floatIntensity={1} floatingRange={[-10, 10]}>
             <mesh
                 ref={brainRef}
                 geometry={(nodes["brain"] as THREE.Mesh).geometry}
-                // rotation={[degToRad(xPos), degToRad(yPos), degToRad(zPos)]}
-                // rotation={[1.5707, 0, 0]}
-                // position={[xPos,yPos,zPos]}
                 position={[180, 20, -20]}
             >
-                <meshStandardMaterial color="#b6b6b6" wireframe />
+                <meshStandardMaterial color="##DDDFDF" wireframe />
             </mesh>
-            <planeHelper args={[planeRef.current, 500, 0xff0000]} />
-        </>
+        </Float>
     );
 }
 
@@ -135,7 +122,7 @@ function BrainBackground() {
         <Canvas
             camera={{
                 fov: 60,
-                position: [0, 0, 360]
+                position: [0, 0, 360],
             }}
         >
             <ambientLight />
